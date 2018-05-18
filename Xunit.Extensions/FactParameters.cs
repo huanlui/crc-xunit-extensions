c69584fadafd0910ecc7b27c58aef52f35f137ea
huanlui <juanluisgarcialopez@outlook.es>
2018-05-18 13:38:24 +0200

    Creo FactParameters para facilitar teste de tipo  [MemberData(nameof(GetSuts))]

---
+++ Xunit.Extensions/FactParameters.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xunit.Extensions
+{
+    /// <summary>
+    /// Useful to create parametrized test data of type [MemberData(nameof(GetSuts))]
+    /// </summary>
+    public class FactParameters : List<object[]>
+    {
+        public void AddCase(params object[] parameters)
+        {
+            Add(parameters);
+        }
+    }
+}
